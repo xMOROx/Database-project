1f4a3ab9a6153c5534435ddddcc19bb8c3d3137d
zajdel0202@gmail.com <zajdel0202@gmail.com>
2023-04-24 13:04:04 +0200

    Init project

---
+++ src/main/java/org/agh/edu/pl/carrentalrestapi/CarRentalRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.agh.edu.pl.carrentalrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/agh/edu/pl/carrentalrestapi/RarRentalRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agh.edu.pl.carrentalrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RarRentalRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
