b4051d8811f878da8cdeb40f818b4bf08ed4e507
xMOROx <57092803+xMOROx@users.noreply.github.com>
2023-05-06 20:39:32 +0200

    Add more flexible pageable request

---
+++ src/main/java/org/agh/edu/pl/carrentalrestapi/utils/PageableRequest.java
@@ -0,0 +1,39 @@
+package org.agh.edu.pl.carrentalrestapi.utils;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(value = PropertyNamingStrategies.UpperCamelCaseStrategy.class)
+public class PageableRequest implements Serializable {
+    public final static long serialVersionUID = 534095743905347L;
+
+    private Integer page;
+
+    private Integer size;
+    
+    public static Pageable toPageable(PageableRequest pageableRequest) {
+        if (Objects.isNull(pageableRequest)) {
+            return Pageable.unpaged();
+        }
+
+        return PageRequest.of(
+                Objects.requireNonNullElse(pageableRequest.getPage(),0),
+                Objects.requireNonNullElse(pageableRequest.getSize(), 100)
+        );
+    }
+}
