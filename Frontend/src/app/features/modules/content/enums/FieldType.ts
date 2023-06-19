a87dacda7f0a1f6b4b831731a4eed9da0085b904
xMOROx <57092803+xMOROx@users.noreply.github.com>
2023-06-19 12:58:23 +0200

    Enums for search post request

---
+++ Frontend/src/app/features/modules/content/enums/FieldType.ts
@@ -0,0 +1,10 @@
+export enum FieldType {
+    BOOLEAN = 'BOOLEAN',
+    INTEGER = 'INTEGER',
+    CHAR = 'CHAR',
+    STRING = 'STRING',
+    DOUBLE = 'DOUBLE',
+    LONG = 'LONG',
+    DATE = 'DATE',
+    BIG_DECIMAL = 'BIGDECIMAL',
+}
